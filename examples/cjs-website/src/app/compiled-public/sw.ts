45c9f430d1da7c9a79caec08a2367a4720802081
Quentin <contact@yovach.dev>
2023-10-28 18:03:23 +0200

    tests: check process.env.NEXT_PUBLIC_* parsing

--- examples/cjs-website/src/app/compiled-public/sw.ts
+++ examples/cjs-website/src/app/compiled-public/sw.ts
@@ -3,7 +3,7 @@ export type {};
 declare let self: ServiceWorkerGlobalScope;
 
 self.addEventListener("install", async () => {
-  console.log("Service worker installed (cjs)");
+  console.log("Service worker installed (%checksum%) on version " + process.env.NEXT_PUBLIC_VERSION);
 });
 
 self.addEventListener("fetch", (evt: FetchEvent) => {
@@ -11,5 +11,9 @@ self.addEventListener("fetch", (evt: FetchEvent) => {
 });
 
 self.addEventListener("error", (err) => {
-  console.error("Service worker error", err);
+  console.error("Service worker error (%checksum%)", err);
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service worker activated (%checksum%)");
 });
--- examples/no-src-folder/tests/check-file.test.cjs
+++ examples/no-src-folder/tests/check-file.test.cjs
@@ -7,6 +7,11 @@ test("no src folder website", async (t) => {
     doesNotReject(readFile("./public/sw.js"));
   });
 
+  await t.test("files should not contains process.env.NEXT_PUBLIC_*", async () => {
+    const fileContent = await readFile("./public/sw.js", "utf8");
+    equal(fileContent.includes("process.env.NEXT_PUBLIC_"), false);
+  });
+
   await t.test("check if file in folder exists", async () => {
     doesNotReject(readFile("./public/folder/random-file.js"));
   });
