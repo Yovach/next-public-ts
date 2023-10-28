3c5ba4506dfcbadbd90206342b15a3973a6593b8
Quentin <contact@yovach.dev>
2023-10-28 13:32:36 +0200

    ref: use glob for inputDir and outputDir options

--- packages/next-public-ts/src/utils.ts
+++ packages/next-public-ts/src/utils.ts
@@ -1,8 +1,9 @@
 import type { Options as SwcOptions } from "@swc/core";
 import { transform } from "@swc/core";
+import { globSync } from "fast-glob";
 import { subtle } from "node:crypto";
 import { existsSync, promises } from "node:fs";
-import { dirname, join as pathJoin, sep as pathSep } from "node:path";
+import { dirname, join as pathJoin } from "node:path";
 
 const encoder = new TextEncoder();
 
@@ -20,7 +21,7 @@ export async function calculateChecksum(fileContent: string): Promise<string> {
 /**
  * Compiles a file with swc and replace %checksum% with the SHA-1 checksum of the file
  */
-export async function compileFile(filePath: string) {
+export async function compileFile(filePath: string): Promise<string> {
   const fileContent = await promises.readFile(filePath, "utf-8");
   const transformed = await transform(fileContent, getSwcOptions());
   transformed.code = transformed.code.replace(
@@ -57,32 +58,27 @@ export async function compileDirectory(
   inputDir: string,
   outputDir: string
 ): Promise<void> {
-  if (!existsSync(inputDir)) {
-    console.warn(`Input directory ${inputDir} does not exist`);
-    return;
-  } else if (!existsSync(outputDir)) {
-    // create output directory if it doesn't exist
-    await promises.mkdir(outputDir, { recursive: true });
-  }
-
-  const files = await promises.readdir(inputDir);
+  const files = globSync([inputDir + "/**/*.ts", "!**/public"]);
   for (const file of files) {
-    const filePath = pathJoin(inputDir, file);
-    const stat = await promises.stat(filePath);
-    if (stat.isDirectory()) {
-      const basename = filePath.split(pathSep).pop()!;
-      return compileDirectory(filePath, pathJoin(outputDir, basename));
-    } else {
-      if (!filePath.endsWith(".ts")) {
-        // ignore non-ts files
-        continue;
-      }
-
-      // write compiled file to output directory
-      const fileContent = await compileFile(filePath);
-      const outputFilePath = pathJoin(outputDir, file.replace(".ts", ".js"));
-      await promises.writeFile(outputFilePath, fileContent);
+    const [, filePath] = file.split("+public/", 2);
+    if (!filePath) {
+      throw new Error("Invalid file path");
     }
+
+    // create output directory if it doesn't exist
+    const outputFilePath = pathJoin(outputDir, filePath.replace(".ts", ".js"));
+    if (!existsSync(outputFilePath)) {
+      await promises.mkdir(dirname(outputFilePath), {
+        recursive: true,
+      });
+    }
+
+    // compile file with swc (from next.js)
+    const inputFilePath = pathJoin(inputDir, filePath);
+    const fileContent = await compileFile(inputFilePath);
+
+    // write compiled file to output directory
+    await promises.writeFile(outputFilePath, fileContent);
   }
 }
 
@@ -96,8 +92,8 @@ export async function compileFiles(inputFiles: string[]): Promise<void> {
       throw new Error("Invalid file path");
     }
 
-    const outputFilePath = pathJoin("public", filePath.replace(".ts", ".js"));
     // create output directory if it doesn't exist
+    const outputFilePath = pathJoin("public", filePath.replace(".ts", ".js"));
     if (!existsSync(outputFilePath)) {
       await promises.mkdir(dirname(outputFilePath), {
         recursive: true,
