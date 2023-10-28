af41b3a645a163271e753d34dec5f6085f2383cd
Quentin <contact@yovach.dev>
2023-10-28 09:56:23 +0200

    feat: using promises from "fs", async webpack hook

--- packages/next-public-ts/src/index.ts
+++ packages/next-public-ts/src/index.ts
@@ -1,16 +1,9 @@
 import { transformSync } from "next/dist/build/swc/index.js";
-import {
-  existsSync,
-  mkdirSync,
-  readFileSync,
-  readdirSync,
-  statSync,
-  writeFileSync,
-} from "node:fs";
+import { existsSync, promises } from "node:fs";
 import { join as pathJoin, sep as pathSep } from "node:path";
-import type { Compiler } from "webpack";
+import { type Compiler } from "webpack";
 
-function compileDirectory(inputDir: string, outputDir: string) {
+async function compileDirectory(inputDir: string, outputDir: string) {
   if (!existsSync(inputDir)) {
     return;
   } else if (!existsSync(outputDir)) {
@@ -18,19 +11,21 @@ function compileDirectory(inputDir: string, outputDir: string) {
     await promises.mkdir(outputDir, { recursive: true });
   }
 
-  const files = readdirSync(inputDir);
+  const files = await promises.readdir(inputDir);
   for (const file of files) {
     const filePath = pathJoin(inputDir, file);
-    const stat = statSync(filePath);
+    const stat = await promises.stat(filePath);
     if (stat.isDirectory()) {
       const basename = filePath.split(pathSep).pop()!;
-      compileDirectory(filePath, pathJoin(outputDir, basename));
+      return compileDirectory(filePath, pathJoin(outputDir, basename));
     } else {
       if (!filePath.endsWith(".ts")) {
         // ignore non-ts files
         continue;
       }
-      const fileContent = readFileSync(filePath, "utf-8");
+      const fileContent = await promises.readFile(filePath, "utf-8");
+
+      // compile file with swc (from next.js)
       const transformed = transformSync(fileContent, {
         jsc: {
           parser: {
@@ -48,8 +43,10 @@ function compileDirectory(inputDir: string, outputDir: string) {
         isModule: true,
         minify: true,
       });
+
+      // write compiled file to output directory
       const outputFilePath = pathJoin(outputDir, file.replace(".ts", ".js"));
-      writeFileSync(outputFilePath, transformed.code);
+      await promises.writeFile(outputFilePath, transformed.code);
     }
   }
 }
@@ -82,11 +79,26 @@ class NextPublicTsPlugin {
   }
 
   apply(compiler: Compiler) {
-    compiler.hooks.afterEmit.tap("NextPublicTsPlugin", (): void => {
-      for (const inputDir of this.inputDir) {
-        compileDirectory(inputDir, this.outputDir);
+    const { webpack } = compiler;
+    const { Compilation } = webpack;
+    compiler.hooks.compilation.tap(
+      "NextPublicTsPlugin",
+      (compilation): void => {
+        compilation.hooks.processAssets.tapPromise(
+          {
+            name: "NextPublicTsPlugin",
+            stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL,
+          },
+          async () => {
+            await Promise.all(
+              this.inputDir.map(async (inputDir) => {
+                return compileDirectory(inputDir, this.outputDir);
+              })
+            );
+          }
+        );
       }
-    });
+    );
   }
 }
 
