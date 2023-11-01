0b393e34f666dc68f12d4deae8d3afd210919c67
Quentin <contact@yovach.dev>
2023-11-01 15:05:32 +0100

    tests: check if js files are handled

--- examples/pages-router/tests/check-file.test.cjs
+++ examples/pages-router/tests/check-file.test.cjs
@@ -7,14 +7,27 @@ test("pages router website", async (t) => {
     doesNotReject(readFile("./public/sw.js"));
   });
 
-  await t.test("files should not contains process.env.NEXT_PUBLIC_*", async () => {
-    const fileContent = await readFile("./public/sw.js", "utf8");
-    equal(fileContent.includes("process.env.NEXT_PUBLIC_"), false);
-  });
+  await t.test(
+    "files should not contains process.env.NEXT_PUBLIC_*",
+    async () => {
+      const fileContent = await readFile("./public/sw.js", "utf8");
+      equal(fileContent.includes("process.env.NEXT_PUBLIC_"), false);
+    },
+  );
 
-  await t.test("check if file in folder exists", async () => {
-    doesNotReject(readFile("./public/folder/random-file.js"));
-  });
+  await t.test(
+    "check if `folder/js-file.js` is handled by the plugin",
+    async () => {
+      doesNotReject(readFile("./public/folder/js-file.js"));
+    },
+  );
+
+  await t.test(
+    "check if `folder/ts-file.js` is handled by the plugin",
+    async () => {
+      doesNotReject(readFile("./public/folder/ts-file.js"));
+    },
+  );
 
   await t.test("check file non existing file", async () => {
     rejects(readFile("./public/non-existent.js"));
