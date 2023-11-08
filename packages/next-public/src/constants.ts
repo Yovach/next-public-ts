81a875d8944a725e1f990d3516869c4e44bdaff7
Quentin <contact@yovach.dev>
2023-11-08 10:24:42 +0100

    feat: add `mts` and `cts` in handled extensions

--- packages/next-public/src/constants.ts
+++ packages/next-public/src/constants.ts
@@ -1,8 +1,11 @@
+const HANDLED_EXTENSIONS = ["ts", "mts", "cts", "js", "mjs", "cjs"] as const;
+
 /**
  * Glob pattern for handled extensions
  * Used to find all files with handled extensions
  */
 export const HANDLED_GLOB_EXTENSIONS = `{${HANDLED_EXTENSIONS.join(",")}}`;
+
 /**
  * Regex pattern for handled extensions
  * Used to replace `.ts` with `.js` in file paths
