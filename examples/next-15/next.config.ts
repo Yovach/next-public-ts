966d9f2599c17195758792552eb63e2246583f5b
Quentin <contact@yovach.dev>
2025-09-02 21:35:35 +0200

    feat: update next-15 example configuration to use NextPublicTsPlugin and remove turbopack options

--- examples/next-15/next.config.ts
+++ examples/next-15/next.config.ts
@@ -1,17 +1,16 @@
 import type { NextConfig } from "next";
+import { NextPublicTsPlugin } from "next-public";
+import path from "node:path";
+import type webpack from "webpack";
 
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
-  turbopack: {
-    rules: {
-      "**/+public/**/*.{js,ts}": {
-        loaders: [
-          {loader: "next-public", options: {}}
-        ],
-        as: "*.js"
-      }
-    }
+  webpack: (config: webpack.Configuration)  => {
+    config.plugins?.push(new NextPublicTsPlugin({
+      inputDir: path.join("app", "+public"),
+      outputDir: path.join("public"),
+    }));
+    return config;
   }
 };
 
